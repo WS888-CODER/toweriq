ae534c08cbb8891e081f5334a5a9d9ceb43c59ba
WS888-CODER <444200088@student.ksu.edu.sa>
2025-07-26 22:31:32 +0000

    ببببببببببب

--- main.py
+++ main.py
@@ -73,6 +73,6 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
-if _name_ == '_main_':
+if _name_ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
